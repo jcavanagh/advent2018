ad56c9f13b23a32826980f5d09e699cd4402ed23
Joe Cavanagh <joe@jlcavanagh.com>
2018-12-18 21:55:55 -0800

    Day 4 - Repose Record

--- src/p04/repose_record.kt
+++ src/p04/repose_record.kt
@@ -1,9 +1,9 @@
 package p04
 
 import common.file.readLines
+import sun.rmi.runtime.Log
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.ArrayList
 
 enum class GuardEventType(val value: String) {
   BEGIN_SHIFT("begins shift"),
@@ -29,22 +29,25 @@ class LogMessage {
     this.guardId = matches.groups.get(3)?.value?.toInt() ?: defaultGuardId
     this.type = GuardEventType.from(matches.groups.get(4)?.value!!)
   }
+
+  override fun toString(): String {
+    return "$dateStr:$minute - $guardId - $type"
+  }
 }
 
-fun mostMinutesAsleep(logs: List<LogMessage>): Pair<Int, Int> {
+fun rangesAsleepByGuardId(logs: List<LogMessage>): Map<Int, List<Pair<Int, Int>>> {
   var lastSleepMinute = 0
-
   val map = mutableMapOf<Int, MutableList<Pair<Int, Int>>>()
   //There's gotta be a cleaner way to deal with a nullable accumulator...
-  logs.groupingBy { it.guardId }.aggregateTo(map) { key, total, msg, first ->
-    if(first) {
+  logs.groupingBy { it.guardId }.aggregateTo(map) { _, total, msg, first ->
+    if (first) {
       mutableListOf()
     } else {
       when (msg.type) {
         GuardEventType.FALL_ASLEEP ->
           lastSleepMinute = msg.minute
         GuardEventType.WAKE_UP ->
-          total!!.add(Pair(lastSleepMinute, msg.minute))
+          total!!.add(Pair(lastSleepMinute, msg.minute - 1))
         GuardEventType.BEGIN_SHIFT -> total!!
       }
 
@@ -52,39 +55,63 @@ fun mostMinutesAsleep(logs: List<LogMessage>): Pair<Int, Int> {
     }
   }
 
-  map.mapValues {
-    it.value.reduce<Int, Pair<Int, Int>> { acc, pair ->
-      acc + pair.first - pair.second
+  return map
+}
+
+fun mostMinutesAsleep(logs: List<LogMessage>): Pair<Int, Int> {
+  val ranges = rangesAsleepByGuardId(logs)
+  val asTotal = ranges.mapValues {
+    it.value.fold(Pair(it.key, 0)) { acc, pair ->
+      val total = acc.second + pair.second - pair.first
+      Pair(it.key, total)
+    }
+  }
+
+  return asTotal.maxBy { foo -> foo.value.second }?.value!!
+}
+
+fun mostSleepyMinuteByGuardId(logs: List<LogMessage>): Map<Int, Pair<Int, Int>> {
+  val ranges = rangesAsleepByGuardId(logs)
+
+  return ranges.mapValues {range ->
+    val minuteCounts = mostSleepyMinuteForRanges(range.value)
+    minuteCounts.maxBy { it.value }?.toPair()!!
   }
 }
 
-fun mostSleepyMinute(guardId: Int, logs: List<LogMessage>): Int {
-  return 0
-}
+fun mostSleepyMinuteForRanges(sleepiestRanges: List<Pair<Int, Int>>): Map<Int, Int> {
+  if(sleepiestRanges.isNotEmpty()) {
+    return sleepiestRanges.flatMap {
+      it.first.rangeTo(it.second)
+    }.groupingBy { it }.eachCount()
+  }
 
-fun guardActivity(logs: List<LogMessage>): Int {
-  val sleepyGuard = mostMinutesAsleep(logs)
-  println(sleepyGuard)
-  val sleepyMinute = mostSleepyMinute(sleepyGuard.first, logs)
-
-  return sleepyGuard.second * sleepyMinute
+  return mutableMapOf(Pair(-1, -1))
 }
 
 fun parseLogDate(dateStr: String): Date {
   return SimpleDateFormat("yyyy-MM-dd HH:mm").parse(dateStr)
 }
 
-fun parseLogs(rawLogs: List<String>): Int {
+fun parseLogs(rawLogs: List<String>): List<LogMessage> {
   var lastGuardId = -1
-  val logs = rawLogs.sortedBy { parseLogDate(it.substringBeforeLast("]").substring(1)) }.map {
+  return rawLogs.sortedBy { parseLogDate(it.substringBeforeLast("]").substring(1)) }.map {
     val msg = LogMessage(it, lastGuardId)
-    if(msg.guardId != -1) lastGuardId = msg.guardId
+    if (msg.guardId != -1) lastGuardId = msg.guardId
     msg
   }
-  return guardActivity(logs)
 }
 
 fun main() {
-  val guardValue = parseLogs(readLines("src/p04/input.txt"))
-  println("Sleepy guard value: $guardValue")
+  val logs = parseLogs(readLines("src/p04/input.txt"))
+  val sleepyGuard = mostMinutesAsleep(logs)
+  val sleepyMinutesByGuard = mostSleepyMinuteByGuardId(logs)
+  val sleepyGuardMinute = sleepyMinutesByGuard[sleepyGuard.first]!!
+  val sleepiestGuardMinute = sleepyMinutesByGuard.maxBy { it.value.second }
+
+  println("Sleepiest guard: $sleepyGuard")
+  println("Sleepiest minute for that guard: $sleepyGuardMinute")
+  println("Sleepy guard value: ${sleepyGuard.first * sleepyGuardMinute.first}")
+  println("Sleepiest overall minute: $sleepiestGuardMinute")
+  println("Sleepiest overall guard value: ${sleepiestGuardMinute?.key!! * sleepiestGuardMinute.value.first}")
 }
